ca04a0d2e3bb82b7ef17be0c8888377c8d4a4ca6
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-13 23:36:35 -0500

    actualizar

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio2{ 
+    
+    public static void main(String[]args){
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Ingresa un texto");
+        String a=leer.next();
+        System.out.println("El texto ingresado es :"+a);
+    }
+
+}
