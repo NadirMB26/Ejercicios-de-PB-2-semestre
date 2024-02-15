89cc0f698edb81ccf49c5b2ac5aab3d03fa279fe
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-14 19:06:37 -0500

    actualizar commit

---
+++ Ejercicio4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ejercicio4 {
+    public static void main(String[]args){
+Scanner leer=new Scanner(System.in);
+double a,b,c;
+System.out.println("Ingresa el diametro");
+a=leer.nextDouble();
+double pi=3.1416;
+b= a/2;
+c=pi*(b*b);
+System.out.println("El area del circulo es: "+c);
+    }
+}
