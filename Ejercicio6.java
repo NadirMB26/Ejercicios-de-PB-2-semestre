456c114d90e3f3a9fc1fed9962a3394d6ac5020a
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-18 00:50:44 -0500

    nuevo ejercicio

---
+++ Ejercicio6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Ejercicio6{
+public static void main(String[
+]args){
+Scanner leer=new Scanner(System.in);
+
+int a,d=3,e=0,r;
+
+for (int i=1; i<=d; i++){
+System.out.println("Digita el numero "+i);
+a=leer.nextInt();
+e+=a;
+System.out.println(e);
+}
+r=e/3;
+System.out.println( "la media aritmética de los numeros ingresados es "+r);
+}
+}
