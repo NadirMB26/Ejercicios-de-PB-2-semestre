2df6eacf5c96811f91731111ed3536a600b322de
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-18 21:24:37 -0500

    Nuevo ejercicio

---
+++ Ejercicio11.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ejercicio11 {
+    public static void main(String[]args){
+Scanner leer=new Scanner(System.in);
+int a,b,c;
+System.out.println("Ingresa la altura");
+a=leer.nextInt();
+System.out.println("Ingresa la base");
+b=leer.nextInt();
+c=a*b;
+System.out.println("El area de tu rectangulo es "+c);
+    }
+}
