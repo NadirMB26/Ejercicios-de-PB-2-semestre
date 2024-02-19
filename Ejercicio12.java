a37e6ffafcfc0373c445211792302e0361afdae3
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-18 21:54:08 -0500

    actualizar

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Ejercicio12 {
     public static void main(String[]args){
 Scanner leer=new Scanner(System.in);
-int a,b,c=0,d=3;
+int a=0,b,c=0,d=3;
 for(int i=1; i<=d; i++){
 System.out.println("Ingresa tu numero ["+i+"]");
 a=leer.nextInt();
@@ -10,8 +10,10 @@ if(i<3){
 System.out.println("El numero elevado es["+(i+1)+"] ="+(a*(i+1)));}
 
 c+=a;
+
     }
-    System.out.println("El numero elevado es["+(4)+"] ="+(a*a));
+  
+    System.out.println("El numero elevado es["+(4)+"] ="+a*a);
     System.out.println("La suma es "+c+" ,el promedio es "+(c/d));
     }
 }
