efc66c9f713ff1f3ade316298e9a0a005bbf8fab
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-02-18 21:12:32 -0500

    nuevo

---
+++ Ejercicio8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Ejercicio8{
+public static void main(String[]args){
+Scanner leer=new Scanner(System.in);
+System.out.println("Digita el numero");
+int a,b;
+a=leer.nextInt();
+b=a/2;
+if(b==1){
+System.out.println("El Numero es entero y su resultado es"+b);
+}else{
+System.out.println("Error El Numero ingresado no es entero");
+}
+}
+}
